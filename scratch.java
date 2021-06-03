c8ec98853336e712cdb7e9790c1ed0782b8ac7b4
Abishaik <69312667+Abishaik@users.noreply.github.com>
2021-06-03 18:49:05 +0530

    Update scratch.java
    stuffs added

--- scratch.java
+++ scratch.java
@@ -1,60 +1,109 @@
 
-// Class  
-public class main{
+// // Class  
+// public class main{
     
-    public static void main(String[] args)
-    {
-        System.out.println("Hello worldd !");
-        // Int
-        int a=100;
-        System.out.println(a);
+//     public static void main(String[] args)
+//     {
+//         System.out.println("Hello worldd !");
+//         // Int
+//         int a=100;
+//         System.out.println(a);
 
-        // Double
-        double f=100.05;
-        System.out.println(f);
+//         // Double
+//         double f=100.05;
+//         System.out.println(f);
 
-        // String
-        String s="Abishaik";
-        System.out.println(s);
+//         // String
+//         String s="Abishaik";
+//         System.out.println(s);
 
-        // Character 
-        char c='c';
-        System.out.println(c);
+//         // Character 
+//         char c='c';
+//         System.out.println(c);
 
-        // Boolean
-        boolean exp=true;
-        System.out.println(exp);
+//         // Boolean
+//         boolean exp=true;
+//         System.out.println(exp);
 
-        // if else statement
-        if(exp)
-        {
-            System.out.println("exp is true");
-        }
-        else{
-            //something
-        }
+//         // if else statement
+//         if(exp)
+//         {
+//             System.out.println("exp is true");
+//         }
+//         else{
+//             //something
+//         }
 
-        // For Loop
-        for(int i=0; i<10; i++)
-        {
-            p("ABISHAIK prints : "+i);
-        }
+//         // For Loop
+//         for(int i=0; i<10; i++)
+//         {
+//             p("ABISHAIK prints : "+i);
+//         }
 
-        // While Loop
-        while(exp)
-        {
-            if(exp)
-            {
-                // Function written below
-                p("This works");
-                break;
-            }
-        } 
+//         // While Loop
+//         while(exp)
+//         {
+//             if(exp)
+//             {
+//                 // Function written below
+//                 p("This works");
+//                 break;
+//             }
+//         } 
 
-    }
-    // Function
-    public static void p(String s)
-    {
-        System.out.println(s);
-    }
-}
+//     }
+//     // Function
+//     public static void p(String s)
+//     {
+//         System.out.println(s);
+//     }
+// }
+// **********************************************************************************************
+
+// OOPS :
+
+//Inheritance 
+
+// class Mobile{  
+//  int Price=40000;  
+// }  
+
+// class ButtonPhone extends Mobile{  
+//  int tax=10000; 
+ 
+//  public static void main(String args[]){  
+//    ButtonPhone b = new ButtonPhone();  
+//    System.out.println(" Price is:"+b.Price);  
+//    System.out.println("Tax is of:"+b.tax);  
+//  }
+// }
+//Abstraction
+
+//Encapsulation
+
+//Polymorphism
+
+// public class mobile {
+
+//    String model;
+//    String name;
+	
+//    mobile(){
+// 	this.name="Realme";
+// 	this.model="Narzo 20 pro";
+//    }
+	
+//    mobile(String n,String a){
+// 	this.name=n;
+// 	this.model=a;
+//    }
+//    public static void main(String args[]){
+// 	mobile obj1 = new mobile();
+// 	mobile obj2 = 
+// 		       new mobile("Iphone", "21 max pro");
+// 	System.out.println(obj1.name+" "+obj1.model);
+// 	System.out.println(obj2.name+" "+obj2.model);
+//    }
+// }
+
+
